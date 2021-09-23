60b4c8540c0d5d54d63c3df32d7bcad86335b384
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-09-24 04:12:22 +0900

    합병정렬

--- week2/mergeSort.py
+++ week2/mergeSort.py
@@ -1,33 +1,59 @@
+import sys
+sys.setrecursionlimit(10000)
+import random, time
+
 def mergeSort(a,l,r):
     if(r > l):
-        m = (r+l)/2
+        m = (r+l)//2
         mergeSort(a,l,m)
         mergeSort(a,m+1,r)
         merge(a,l,m,r)
 
-def merge(a,low, mid, high):
-    temp = []
-    l, h = low, mid
-    while l < mid and h < high:
-        if a[l] < a[h]:
-            temp.append(a[l])
-            l += 1
+def merge(a,l, m, r):
+    i=l
+    j=m+1
+    k=l
+    b=a.copy()
+    while((i<=m)and(j<=r)):
+        if(a[i]<a[j]):
+            b[k]=a[i]
+            k+=1
+            i+=1
         else:
-            temp.append(a[h])
-            h += 1
+            b[k]=a[j]
+            k+=1
+            j+=1
+    if(i>m):
+        for p in range (j, r+1):
+            b[k]=a[p]
+            k+=1
+    else :
+        for p in range (i,m+1):
+            b[k]=a[p]
+            k+=1
+    for p in range (l,r+1):
+        a[p]=b[p]
 
-    while l < mid:
-        temp.append(a[l])
-        l += 1
-    while h < high:
-        temp.append(a[h])
-        h += 1
+def checkSort(a, n):
+    isSorted = True
+    for i in range(1, n):
+        if a[i] > a[i+1]:
+            isSorted = False
+        if not isSorted:
+            break
+    if isSorted:
+        print("정렬 완료")
+    else:
+        print("정렬 오류 발생")
 
-    for i in range(low, high):
-        a[i] = temp[i - low]
-    return a
 
-a= [6,2,8,1,3,9,4,5,10,7]
-mergeSort(a,0,9)
-for i in range(10):
-    print(a[i], end=' ')
+N = 100000
+a = []
+a.append(None)
+for i in range(N):
+    a.append(random.randint(1, N))
+start_time = time.time()
+mergeSort(a,1,N)
+end_time = time.time() - start_time
+print("선택 정렬의 실행 시간 (N=%d) : %0.3f"%(N, end_time))
+checkSort(a, N)
