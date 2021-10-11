c82c82917a70067efc50509fe5da7e7c4305b99e
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-10-12 01:16:20 +0900

    버블정렬

--- week2/bubbleSort.py
+++ week2/bubbleSort.py
@@ -1,5 +1,5 @@
 def bubbleSort(a, n):
-    for i in range(n, 2, -1):
+    for i in range(n, 0, -1):
         for j in range(1, i):
             if(a[j]>a[j+1]):
                 temp = a[j]
@@ -7,7 +7,31 @@ def bubbleSort(a, n):
                 a[j+1]=temp
     return a
 
-A = [3,2,5,1,4]
-res = bubbleSort(A, len(A)-1)
-for i in range(len(A)):
-    print(res[i],end=' ')
+def checkSort(a, n):
+
+    isSorted = True
+    for i in range(1, n):
+        if a[i] > a[i+1]:
+            isSorted = False
+        if not isSorted:
+            break
+    if isSorted:
+        print("정렬 완료")
+    else:
+        print("정렬 오류 발생")
+
+
+import random, time
+
+
+N = 5000
+a = []
+a.append(None)
+
+for i in range(N):
+    a.append(random.randint(1, N))
+start_time = time.time()
+bubbleSort(a, N)
+end_time = time.time() - start_time
+print("버블 정렬의 실행 시간 (N=%d) : %0.3f"%(N, end_time))
+checkSort(a, N)
