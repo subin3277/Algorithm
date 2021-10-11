a905ebbfbbc145d13bdfbe49c7a9e44227d4c2bb
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-10-12 01:40:42 +0900

    교환정렬

---
+++ homework/exchangesort.py
@@ -0,0 +1,36 @@
+def exchangeSort(a,n):
+    for i in range(1,n):
+        for j in range(i+1,n+1):
+            if(a[i]<a[j]):
+                temp = a[i]
+                a[i]=a[j]
+                a[j]=temp
+    return a
+
+def checkSort(a, n):
+    isSorted = True
+    for i in range(1, n):
+        if a[i] < a[i+1]:
+            isSorted = False
+        if not isSorted:
+            break
+    if isSorted:
+        print("정렬 완료")
+    else:
+        print("정렬 오류 발생")
+
+
+import random, time
+
+N = 5000
+a = []
+a.append(None)
+
+for i in range(N):
+    a.append(random.randint(1, N))
+start_time = time.time()
+exchangeSort(a, N)
+end_time = time.time() - start_time
+print("교환 정렬의 실행 시간 (N=%d) : %0.3f"%(N, end_time))
+checkSort(a, N)
+
